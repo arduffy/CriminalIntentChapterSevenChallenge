59a0c8175c9f8c466ad339d2732a6f7beb2a0068
Amanda <arduffy>
2018-02-05 13:04:24 -0600

    Book Work to 141

---
+++ app/src/main/java/com/cidm4385/wt/criminalintent/Crime.java
@@ -0,0 +1,47 @@
+package com.cidm4385.wt.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Amanda on 2/5/2018.
+ */
+
+public class Crime {
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
