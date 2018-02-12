e396a8298a9530b388ac8fdea83ddf7bed235376
Amanda <arduffy>
2018-02-12 12:51:48 -0600

    Chapter 7 Challenge Complete

--- app/src/main/java/com/cidm4385/wt/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/cidm4385/wt/criminalintent/CrimeActivity.java
@@ -1,26 +1,38 @@
 package com.cidm4385.wt.criminalintent;
 
+import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.app.Fragment;
+//import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
-        FragmentManager fm = getSupportFragmentManager();
+        FragmentManager fm = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-        if (fragment == null) {
-            fragment = new CrimeFragment();
+        if(fragment == null){
+            fragment = new Fragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+
+//        FragmentManager fm = getFragmentManager();
+//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+//
+//        if (fragment == null) {
+//            fragment = new CrimeFragment();
+//            fm.beginTransaction()
+//                    .add(R.id.fragment_container, fragment)
+//                    .commit();
+//        }
     }
 }
 
